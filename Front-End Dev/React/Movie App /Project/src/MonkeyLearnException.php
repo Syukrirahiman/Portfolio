e8dbc408866ab313eaac6430632e682fa572c172
Muhammad Syukri Bin Rahiman <muhammadsr.2017@smu.edu.sg>
2019-12-17 20:24:39 +0800

    Delete movie app

--- Front-End Dev/React/Movie App /Project/src/MonkeyLearnException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace MonkeyLearn;
-
-class MonkeyLearnException extends \Exception {
-    public function __construct($message = null, $code = 0, \Exception $previous = null){
-        parent::__construct('Error: '.$message, $code, $previous);
-    }
-}
-?>
